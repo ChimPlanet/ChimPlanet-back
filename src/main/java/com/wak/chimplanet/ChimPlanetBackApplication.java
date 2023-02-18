dd5f50ce622fc1d0048d88371c926f2df322e1e3
ch200203 <ch200205@naver.com>
2023-02-18 21:33:17 +0900

    init : project init

---
+++ src/main/java/com/wak/chimplanet/ChimPlanetBackApplication.java
@@ -0,0 +1,13 @@
+package com.wak.chimplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimPlanetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChimPlanetBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wak/chimplanet/ChimPlanetBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wak.chimplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimPlanetBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
