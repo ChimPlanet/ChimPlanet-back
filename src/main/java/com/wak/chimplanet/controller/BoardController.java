fcf7af3a5337c0ffd763ee1905aeec0c69f07e2b
ch200203 <ch200205@naver.com>
2023-02-27 22:59:19 +0900

    feat : 전체게시판 조회 응답 생성(test용)

--- src/main/java/com/wak/chimplanet/controller/BoardController.java
+++ src/main/java/com/wak/chimplanet/controller/BoardController.java
@@ -1,13 +1,29 @@
 package com.wak.chimplanet.controller;
 
+import com.wak.chimplanet.entity.Board;
+import com.wak.chimplanet.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class BoardController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final BoardService boardService;
 
     @GetMapping("/")
     public String testController() {
         return "test";
+    }
+
+    @GetMapping("/api/boards")
+    public List<Board> getAllBoardList() {
+        return boardService.getAllBoardList();
     }
 }
--- src/main/java/com/wak/chimplanet/service/BoardService.java
+++ src/main/java/com/wak/chimplanet/service/BoardService.java
@@ -1,4 +1,20 @@
 package com.wak.chimplanet.service;
 
+import com.wak.chimplanet.batch.NaverCafeCrawler;
+import com.wak.chimplanet.entity.Board;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BoardService {
+    private final NaverCafeCrawler naverCafeCrawler;
+
+    public List<Board> getAllBoardList() {
+        return naverCafeCrawler.getNaverCafeBoard();
+    }
 }
