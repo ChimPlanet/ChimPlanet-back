519c96e3c5f470150b2038f96d19caeab997d0a8
ch200203 <ch200205@naver.com>
2023-02-28 10:09:05 +0900

    fix : CORS ORIGIN 수정, regDate writer 오타 수정

--- src/main/java/com/wak/chimplanet/controller/BoardController.java
+++ src/main/java/com/wak/chimplanet/controller/BoardController.java
@@ -5,6 +5,7 @@ import com.wak.chimplanet.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,7 @@ public class BoardController {
     }
 
     @GetMapping("/api/boards")
+    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:80"})
     public List<Board> getAllBoardList() {
         return boardService.getAllBoardList();
     }
