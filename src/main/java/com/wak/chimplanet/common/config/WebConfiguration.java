57bb0d17ebbbc5f80c54b2ed240064630d7898f3
ch200203 <ch200205@naver.com>
2023-03-16 13:17:18 +0900

    feat :  이미지파일 처리를 위한 ResourceHandlers 추가

--- src/main/java/com/wak/chimplanet/common/config/WebConfiguration.java
+++ src/main/java/com/wak/chimplanet/common/config/WebConfiguration.java
@@ -2,6 +2,7 @@ package com.wak.chimplanet.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,11 @@ public class WebConfiguration implements WebMvcConfigurer {
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
             .allowCredentials(false).maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("file:/home/wak/images/");
     }
 }
