7f29068ff12b4a63c5941943cbf67b94f1c05877
ch200203 <ch200205@naver.com>
2023-03-16 18:13:38 +0900

    feat : 이미지파일 전달 URI 생성

--- src/main/java/com/wak/chimplanet/service/FileService.java
+++ src/main/java/com/wak/chimplanet/service/FileService.java
@@ -7,12 +7,14 @@ import com.wak.chimplanet.repository.FileRepository;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Service
 @Transactional
@@ -23,23 +25,31 @@ public class FileService {
     private final FileRepository fileRepository;
     @Value("${path.upload-images}")
     private String filePath;
-    // private final String filePath = "src/main/resources/static/images/";
-    
+
+    /**
+     * 이미지 전체 조회하기
+     */
     public List<FileEntity> findAllImages() {
         return fileRepository.findAll();
     }
 
     /**
      * 이미지 업로드
-     * @return Map<String, Object>
+     * 추후 DTO 로 리팩토링 변경예정
      */
     public FileEntity uploadImage(MultipartFile[] files, ImageType imageType, String useYn,
-        DeviceType deviceType, String redirectUrl) {
+        DeviceType deviceType, String redirectUrl, int sequence) {
         String fileNames = "";
 
         String originFileName = files[0].getOriginalFilename();
         long fileSize = files[0].getSize();
-        String safeFile = System.currentTimeMillis() + originFileName; // 고유 파일명
+        String safeFile = UUID.randomUUID().toString() + "-" + originFileName; // 고유 파일명
+
+        String fileUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("images/")// 리소스 핸들러 명칭
+            .path(safeFile)
+            .toUriString();
+
         File file = new File(filePath + File.separator + safeFile);
 
         log.info("originFileName: {}, fileSize: {}, safeFileName: {}, filePath: {}",
@@ -58,8 +68,17 @@ public class FileService {
             .redirectUrl(redirectUrl)
             .useYn(useYn)
             .imageType(imageType)
+            .imageUri(fileUri)
+            .sequence(sequence)
             .build();
 
         return fileRepository.save(fileEntity);
+    }
+
+    public FileEntity updateImage(Long fileId, MultipartFile file) {
+        /**
+         * 구현예정
+         */
+        return null;
     }
 }
