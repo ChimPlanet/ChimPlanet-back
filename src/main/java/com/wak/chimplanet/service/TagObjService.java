635dc22408e73d0d982f19cc9e1c6b02045b7d3c
phg <phg@kopens.com>
2023-03-26 20:59:52 +0900

    Refactor 필드주입 -> 생성자주입

--- src/main/java/com/wak/chimplanet/service/TagObjService.java
+++ src/main/java/com/wak/chimplanet/service/TagObjService.java
@@ -2,7 +2,6 @@ package com.wak.chimplanet.service;
 
 import com.wak.chimplanet.entity.TagObj;
 import com.wak.chimplanet.repository.TagObjRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +10,12 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 public class TagObjService {
-    @Autowired
-    TagObjRepository tagObjRepository;
+    private final TagObjRepository tagObjRepository;
+
+    public TagObjService(TagObjRepository tagObjRepository) {
+        this.tagObjRepository = tagObjRepository;
+    }
+
     public List<TagObj> getTagList(TagObj tagObj) {
 
         return tagObjRepository.findALl();
