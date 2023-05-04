777bc7e4400d8889c85d53cd8c17a388c4890043
ch200203 <ch200205@naver.com>
2023-05-04 23:57:04 +0900

    fix : spring 기본 실행 profile prod 지정 및 로그파일 생성

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -24,6 +24,7 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+# nohup java -jar -Dspring.profiles.active=prod -Dserver.port=${TARGET_PORT} $JAR_PATH > /dev/null 2> /dev/null < /dev/null & >
+nohup java -jar -Dspring.profiles.active=prod -Dserver.port=${TARGET_PORT} $JAR_PATH > /home/ec2-user/log 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
