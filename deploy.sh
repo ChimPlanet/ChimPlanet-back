e9185a80e628042545bcf341caa5b7d829183152
MinjuKim <kpoong02@naver.com>
2023-07-23 15:52:27 +0900

    feat: 무중단배포 중지

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+REPOSITORY=/home/ec2-user/chimplanet
+cd $REPOSITORY
+
+APP_NAME=chimplanet-back
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  sudo kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
